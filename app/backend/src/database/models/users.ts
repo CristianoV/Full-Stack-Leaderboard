f2855e3b45f74c5572e01fd8adc9da1afafe859c
CristianoV <cristianoviieira@gmail.com>
2022-09-28 14:43:56 -0300

    feat: adiona nova classe que retorna do banco de dados dados sobre o usuario

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,42 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string ;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
