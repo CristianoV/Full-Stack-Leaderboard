4649da2f4c93489bc725fab056d73ded5ed6e27f
CristianoV <cristianoviieira@gmail.com>
2022-09-29 00:24:05 -0300

    feat: adiciona nova função que filtra a autorização e envia os dados da role do usuario

--- app/backend/src/controllers/user/userController.ts
+++ app/backend/src/controllers/user/userController.ts
@@ -12,6 +12,16 @@ export default class UserController {
   async login(req: Request, res: Response) {
     const { email, password } = req.body;
     const user = await this.userService.login(email, password);
+    if (user instanceof Error) {
+      return res.status(user.statusCode).json({ message: user.message });
+    }
     return res.status(200).json(user);
+  }
+
+  static async validade(req: Request, res: Response) {
+    const { authorization } = req.headers;
+
+    const valid = UserService.validateLogin(authorization);
+    return res.status(200).json(valid);
   }
 }
