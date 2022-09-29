3aa52e2dbfd53aefcd3e0bed62e9bbe4b367c554
CristianoV <cristianoviieira@gmail.com>
2022-09-28 21:20:08 -0300

    feat: adiciona nova rota para usuarios poderem fazer login

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import UserRoutes from './rotes/UserRoutes';
 
 class App {
   public app: express.Express;
@@ -22,6 +23,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    this.app.use(UserRoutes);
   }
 
   public start(PORT: string | number):void {
