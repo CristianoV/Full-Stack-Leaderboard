733ebfbfffc35f70a0ecfad97ee5586f5da91959
CristianoV <cristianoviieira@gmail.com>
2022-09-29 21:21:50 -0300

    fix: removendo arquivos desnecessarios

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -4,11 +4,6 @@ import UserService from '../services/user.service';
 export default class UserController {
   constructor(private userService: UserService) {}
 
-  async create(req: Request, res: Response) {
-    const user = await this.userService.create(req.body);
-    res.status(201).json(user);
-  }
-
   async login(req: Request, res: Response) {
     const { email, password } = req.body;
     const user = await this.userService.login(email, password);
