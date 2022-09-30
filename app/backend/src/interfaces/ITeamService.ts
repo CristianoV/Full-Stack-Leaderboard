2fe1d82d599a4a43bbbe5a199f2a91e507f21b82
CristianoV <cristianoviieira@gmail.com>
2022-09-30 09:35:08 -0300

    feat: adiciona interface para service

--- app/backend/src/interfaces/ITeamService.ts
+++ app/backend/src/interfaces/ITeamService.ts
@@ -1,6 +1,9 @@
+import Teams from '../database/models/teams';
+
 interface ITeamService {
   // create(obj: UserDTO): Promise<User>,
   allTeams(): any,
+  findById(id: string): Promise<Teams>,
 }
 
 export default ITeamService;
