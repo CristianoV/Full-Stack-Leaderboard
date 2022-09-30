90de6479eb01f53ad92a2d1c37d8ece52051070c
CristianoV <cristianoviieira@gmail.com>
2022-09-29 21:20:29 -0300

    fix: removendo arquivos desnecessarios

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,20 +1,10 @@
 import PersonalError from '../utils/PersonalError';
 import JwtSecret from '../utils/JwtService';
-import UserDTO from '../controllers/dto/UserDTO';
-// import { UserDTO, UserDTOValidation } from '../controllers/user/dto/UserDTO';
-import User from '../database/models/users';
-// import IModel from '../interfaces/IModel';
 import IUserService from '../interfaces/IUserService';
 import BcryptService from '../utils/BcriptService';
 
 class UserService implements IUserService {
   constructor(private userModel: any) { }
-
-  async create(obj: UserDTO): Promise<User> {
-    // await UserDTOValidation.validateAsync(obj);
-    const passwordHash = BcryptService.encrypt(obj.password);
-    return this.userModel.create({ passwordHash, ...obj });
-  }
 
   public async login(email: string, password: string) {
     if (!email || !password) return new PersonalError('All fields must be filled', 400);
