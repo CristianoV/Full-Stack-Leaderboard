108eee749e4c0bf036ba6f25b17df052ac974585
CristianoV <cristianoviieira@gmail.com>
2022-09-30 21:37:38 -0300

    feat: adiciona camada de service que se comunica com o servidor e filtra as partidas

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,73 @@
+import { INTEGER, Model } from 'sequelize';
+import Teams from './teams';
+import db from '.';
+
+class Matche extends Model {
+  id: number;
+  homeTeam!: string;
+  homeTeamGoals!: string;
+  awayTeam!: string;
+  awayTeamGoals!: string;
+  inProgress!: string;
+}
+
+Matche.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: INTEGER,
+    },
+    homeTeam: {
+      type: INTEGER,
+      references: {
+        model: 'teams',
+        key: 'team_name',
+      },
+    },
+    homeTeamGoals: INTEGER,
+    awayTeam: {
+      type: INTEGER,
+      references: {
+        model: 'teams',
+        key: 'team_name',
+      },
+    },
+    awayTeamGoals: INTEGER,
+    inProgress: INTEGER,
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'matche',
+    timestamps: false,
+  },
+);
+
+// Teams.hasMany(Matche, { foreignKey: 'homeTeam', as: 'home_team' });
+// Teams.hasMany(Matche, { foreignKey: 'awayTeam', as: 'away_team' });
+// Matche.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'home_team' });
+// Matche.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'away_team' });
+Matche.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matche.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Matche;
+
+// const teste = async () => {
+//   const ab = await Matche.findAll({
+//     include: [
+//       {
+//         model: Teams,
+//         as: 'home_team',
+//       },
+//       {
+//         model: Teams,
+//         as: 'away_team',
+//       },
+//     ],
+//   });
+//   console.log(ab);
+// };
+
+// teste();
