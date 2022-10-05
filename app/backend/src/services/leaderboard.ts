1995ed684b6aa9b9ee72eea3ab8c1fe4e2d6f8b7
CristianoV <cristianoviieira@gmail.com>
2022-10-04 23:25:50 -0300

    feat: nova classe adicionada que controla a camada de service da rota de leaderboard, filtrando dados e calculando uma pontuação de uma tabela

---
+++ app/backend/src/services/leaderboard.ts
@@ -0,0 +1,124 @@
+import ILeaderboard from '../interfaces/ILeaderboard';
+import Teams from '../database/models/teams';
+import Matche from '../database/models/matches';
+
+export default class Leaderboards {
+  public static async getAllLeaderboard() {
+    const matchTeamAway = await Matche.findAll({ raw: true,
+      where: { inProgress: false },
+      include: [{ model: Teams, as: 'teamAway' }] });
+    const matchTeamHome = await Matche.findAll({ raw: true,
+      where: {
+        inProgress: false },
+      include: [{ model: Teams, as: 'teamHome' }] });
+
+    const home = await Leaderboards.getHome(matchTeamHome) as unknown as ILeaderboard[];
+    const away = await Leaderboards.getAway(matchTeamAway) as unknown as ILeaderboard[];
+    const all = [...home, ...away];
+    const leaderboard = Leaderboards.getLeaderboard(all);
+
+    return leaderboard;
+  }
+
+  public static async getHomeLeaderboard() {
+    const matchTeamHome = await Matche.findAll({ raw: true,
+      where: {
+        inProgress: false },
+      include: [{ model: Teams, as: 'teamHome' }] });
+
+    const home = await Leaderboards.getHome(matchTeamHome) as unknown as ILeaderboard[];
+    const leaderboard = Leaderboards.getLeaderboard(home);
+    return leaderboard;
+  }
+
+  public static async getAwayLeaderboard() {
+    const matchTeamAway = await Matche.findAll({ raw: true,
+      where: { inProgress: false },
+      include: [{ model: Teams, as: 'teamAway' }] });
+    const away = await Leaderboards.getAway(matchTeamAway) as unknown as ILeaderboard[];
+    const leaderboard = Leaderboards.getLeaderboard(away);
+
+    return leaderboard;
+  }
+
+  private static async getAway(match: any) {
+    const xablau = match.map((item: any) => {
+      const pontos = { vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+
+      if (item.homeTeamGoals < item.awayTeamGoals) pontos.vitorias += 1;
+      if (item.homeTeamGoals > item.awayTeamGoals) pontos.derrotas += 1;
+      if (item.homeTeamGoals === item.awayTeamGoals) pontos.empates += 1;
+
+      return { name: item['teamAway.teamName'],
+        totalPoints: pontos.empates * 1 + pontos.vitorias * 3,
+        totalGames: 1,
+        totalVictories: pontos.vitorias,
+        totalDraws: pontos.empates,
+        totalLosses: pontos.derrotas,
+        goalsFavor: item.awayTeamGoals,
+        goalsOwn: item.homeTeamGoals,
+        goalsBalance: item.awayTeamGoals - item.homeTeamGoals,
+      };
+    });
+    return xablau;
+  }
+
+  private static async getHome(match: any) {
+    const xablau = match.map((item: any) => {
+      const pontos = { vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+
+      if (item.homeTeamGoals > item.awayTeamGoals) pontos.vitorias += 1;
+      if (item.homeTeamGoals < item.awayTeamGoals) pontos.derrotas += 1;
+      if (item.homeTeamGoals === item.awayTeamGoals) pontos.empates += 1;
+
+      return { name: item['teamHome.teamName'],
+        totalPoints: pontos.empates * 1 + pontos.vitorias * 3,
+        totalGames: 1,
+        totalVictories: pontos.vitorias,
+        totalDraws: pontos.empates,
+        totalLosses: pontos.derrotas,
+        goalsFavor: item.homeTeamGoals,
+        goalsOwn: item.awayTeamGoals,
+        goalsBalance: item.homeTeamGoals - item.awayTeamGoals,
+      };
+    });
+    return xablau;
+  }
+
+  private static async getLeaderboard(all: ILeaderboard[]) {
+    const novoLista = all.reduce((soma: ILeaderboard[], cur: ILeaderboard) => {
+      const { name } = cur;
+      const repetido = soma.find((elem) => elem.name === name);
+
+      if (repetido) {
+        repetido.totalPoints += cur.totalPoints;
+        repetido.totalGames += 1;
+        repetido.totalVictories += cur.totalVictories;
+        repetido.totalDraws += cur.totalDraws;
+        repetido.totalLosses += cur.totalLosses;
+        repetido.goalsFavor += cur.goalsFavor;
+        repetido.goalsOwn += cur.goalsOwn;
+        repetido.goalsBalance += cur.goalsBalance;
+      } else soma.push(cur);
+      return soma;
+    }, []);
+
+    return this.ordem(novoLista);
+  }
+
+  private static ordem(teste: any) {
+    const amen = teste.map((game: ILeaderboard) => ({
+      ...game,
+      efficiency: ((game.totalPoints / (game.totalGames * 3)) * 100).toFixed(2),
+    }));
+
+    const queijo = amen.sort((a: ILeaderboard, b: ILeaderboard) => (
+      b.totalPoints - a.totalPoints
+      || b.totalVictories - a.totalVictories
+      || b.goalsBalance - a.goalsBalance
+      || b.goalsFavor - a.goalsFavor
+      || a.goalsOwn - b.goalsOwn
+    ));
+    return queijo;
+  }
+}
