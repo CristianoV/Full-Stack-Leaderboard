42a7f0da6b4e39dcd22a16724b84de0b10be6028
CristianoV <cristianoviieira@gmail.com>
2022-10-05 20:49:59 -0300

    feat: novos mocks para os testes

--- app/backend/src/tests/utils/mocks.ts
+++ app/backend/src/tests/utils/mocks.ts
@@ -109,7 +109,7 @@ const matcheNotInProgressMock = [
     homeTeamGoals: 1,
     awayTeam: 14,
     awayTeamGoals: 1,
-    inProgress: 0,
+    inProgress: 1,
     teamHome: {
       id: 9,
       teamName: 'Internacional',
@@ -168,6 +168,99 @@ export const newMatcheCreatedMock = {
     "awayTeamGoals": 2,
     "inProgress": true,
   }
+
+  const teste = {
+    dataValues: {
+      id: 83,
+      homeTeam: 6,
+      homeTeamGoals: 1,
+      awayTeam: 2,
+      awayTeamGoals: 1,
+      inProgress: 0
+    },};
+
+export const BoardMock = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    'teamHome.id': 16,
+    'teamHome.teamName': 'São Paulo'
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    'teamHome.id': 9,
+    'teamHome.teamName': 'Internacional'
+  },
+  {
+    id: 3,
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: 0,
+    'teamHome.id': 4,
+    'teamHome.teamName': 'Corinthians'
+  },
+  {
+    id: 4,
+    homeTeam: 3,
+    homeTeamGoals: 0,
+    awayTeam: 2,
+    awayTeamGoals: 0,
+    inProgress: 0,
+    'teamHome.id': 3,
+    'teamHome.teamName': 'Botafogo'
+  },
+  {
+    id: 5,
+    homeTeam: 7,
+    homeTeamGoals: 1,
+    awayTeam: 10,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    'teamHome.id': 7,
+    'teamHome.teamName': 'Flamengo'
+  },
+  {
+    id: 6,
+    homeTeam: 5,
+    homeTeamGoals: 1,
+    awayTeam: 13,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    'teamHome.id': 5,
+    'teamHome.teamName': 'Cruzeiro'
+  },
+  {
+    id: 7,
+    homeTeam: 12,
+    homeTeamGoals: 2,
+    awayTeam: 6,
+    awayTeamGoals: 2,
+    inProgress: 0,
+    'teamHome.id': 12,
+    'teamHome.teamName': 'Palmeiras'
+  },
+  {
+    id: 8,
+    homeTeam: 15,
+    homeTeamGoals: 0,
+    awayTeam: 1,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    'teamHome.id': 15,
+    'teamHome.teamName': 'São José-SP'
+  },
+]
 
 export {
   Login,
